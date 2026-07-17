de1f37f92e789e01b4d9183ae9c0b7ae99ff0f91
Nookala Varshini <nvarshini1101@gmail.com>
2026-07-17 14:00:57 +0530

    trial of html

---
+++ src/test/java/com/Varshini/AutoSuggest/AutoSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Varshini.AutoSuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
